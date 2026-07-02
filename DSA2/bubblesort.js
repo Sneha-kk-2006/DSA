89004c1997290bc78cfbf0d989c6f6253cae9c5f
Sneha KK <snehakk142006@gmail.com>
2026-07-02 13:39:17 +0530

    all push in to the DSA1 folder

---
+++ DSA2/bubblesort.js
@@ -0,0 +1,90 @@
+// let a=[5, 1, 4, 2, 8];
+
+// let a= [4, 2, 5, 2, 1, 4]
+
+//ascending order
+// for(let i=0;i<a.length;i++){
+//     for(let j=0;j<a.length;j++){
+//         if(a[j]>a[i]){
+//             let temp=a[i];
+//             a[i]=a[j]
+//             a[j]=temp
+//         }
+//     }
+// }
+
+// console.log(a)
+
+
+//descending order
+// for(let i=0;i<a.length;i++){
+//     for(let j=0;j<a.length;j++){
+//         if(a[j]<a[i]){
+//             let temp=a[i];
+//             a[i]=a[j]
+//             a[j]=temp
+//         }
+//     }
+// }
+
+// console.log(a)
+
+
+
+//Count the number of swaps performed during Bubble Sort.
+
+
+// let a= [5, 3, 2]
+// let s=0;
+// for(let i=0;i<a.length;i++){
+//     for(let j=0;j<a.length;j++){
+//         if(a[j]>a[i]){
+//             s++;
+//             let temp=a[i];
+//             a[i]=a[j]
+//             a[j]=temp
+//         }
+//     }
+// }
+
+// console.log(s)
+
+
+
+let a= [1, 2,7, 3, 4, 5]
+
+
+let s=0;
+let swap=false
+for(let i=0;i<a.length;i++){
+    swap=false;
+    for(let j=0;j<a.length;j++){
+       
+        if(a[j]>a[i]){
+            s++;
+            let temp=a[i];
+            a[i]=a[j]
+            a[j]=temp
+             swap= true
+        }
+    }
+
+        
+     if(!swap){
+  console.log("already sorted");
+  break;
+     }
+      
+}
+if(swap){
+console.log(s)
+}
+
+
+
+
+
+
+
+
+
